2f5df695429ddfc5453575e8b4a4feb7d96a14f6
Белла <xhxhvxs@bxq.jsx>
2024-12-19 19:59:21 +0300

    Fixed bug #123

--- src/main/java/org/example/ProductPackaging.java
+++ src/main/java/org/example/ProductPackaging.java
@@ -1,4 +1,43 @@
 package org.example;
 
+import java.util.Objects;
+
 public class ProductPackaging {
+    private String name;
+    private double weight;
+
+    public ProductPackaging(String name, double weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+    public String getName()
+    {
+        return name;
+    }
+    public double getWeight()
+    {
+        return weight;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductPackaging that = (ProductPackaging) o;
+        return Double.compare(weight, that.weight) == 0 && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, weight);
+    }
+
+    @Override
+    public String toString() {
+        return "ProductPackaging{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
 }
