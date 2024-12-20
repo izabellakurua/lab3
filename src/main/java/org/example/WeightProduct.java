b3213f087adbe0446f5a6a891ab004224db6f0e7
Белла <xhxhvxs@bxq.jsx>
2024-12-20 14:37:53 +0300

    write a class WeightProduct

---
+++ src/main/java/org/example/WeightProduct.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class WeightProduct extends Product {
+    public WeightProduct(String name, String description)
+    {
+
+        super(name, description);
+
+    }
+}
